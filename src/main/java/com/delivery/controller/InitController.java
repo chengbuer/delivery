fe29dc84d1bd215219d21212a9b9d6098a09bf4d
chengbuer <jianzhidongnan@163.com>
2019-02-24 21:27:08 +0800

    20190224 晚

--- src/main/java/com/delivery/controller/InitController.java
+++ src/main/java/com/delivery/controller/InitController.java
@@ -1,6 +1,8 @@
 package com.delivery.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Administrator
@@ -11,6 +13,11 @@ import org.springframework.stereotype.Controller;
  */
 
 @Controller
+@RequestMapping("/map")
 public class InitController {
     // 初始化地图界面
+    @RequestMapping(value = "/index", method= RequestMethod.GET)
+    public String init(){
+        return "index";
+    }
 }
---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,82 @@
+var bMap = {
+    URL:{
+        pointInterest:function(){
+            return "/delivery/pointInterest";
+        },
+
+        task:function(){
+            return "/delivery/task";
+        },
+
+        worker:function(){
+            return "/delivery/worker";
+        },
+
+        workerImage:function(){
+            return "https://t1.picb.cc/uploads/2018/11/17/JXb8n6.png";
+        }
+    },
+
+    init:function (map) {
+
+        // 创建Map实例
+        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+        //添加地图类型控件
+        map.addControl(new BMap.MapTypeControl({
+            mapTypes:[
+                BMAP_NORMAL_MAP,
+                BMAP_HYBRID_MAP
+            ]}));
+        map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+        map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    },
+
+    initPointInterest:function(map){
+        var bounds = map.getBounds();
+        var sw = bounds.getSouthWest();
+        var ne = bounds.getNorthEast();
+
+
+        $.post(bMap.URL.pointInterest(),  {lngS: sw.lng, lngE: ne.lng, latS: sw.lat, latE:ne.lat}, function (points) {
+
+            for (var i = 0; i < points.length; i ++) {
+                var point = new BMap.Point(points[i].lng, points[i].lat);
+
+                var myIcon = new BMap.Icon(bMap.URL.workerImage(), new BMap.Size(300,157));
+                var marker = new BMap.Marker(point, {icon:myIcon});
+
+                map.addOverlay(marker);
+            }
+
+
+        });
+    },
+
+    initTask:function(map){
+        var bounds = map.getBounds();
+        var sw = bounds.getSouthWest();
+        var ne = bounds.getNorthEast();
+
+        $.post(bMap.URL.task(), {lngS: sw.lng, lngE: ne.lng, latS: sw.lat, latE:ne.lat}, function (points) {
+            for (var i = 0; i < points.length; i ++) {
+                var point = new BMap.Point(points[i].lng, points[i].lat);
+                var marker = new BMap.Marker(point);
+                map.addOverlay(marker);
+            }
+        });
+    },
+
+    initWorker:function(map){
+        var bounds = map.getBounds();
+        var sw = bounds.getSouthWest();
+        var ne = bounds.getNorthEast();
+
+        $.post(bMap.URL.worker(), {lngS: sw.lng, lngE: ne.lng, latS: sw.lat, latE:ne.lat}, function (points) {
+            for (var i = 0; i < points.length; i ++) {
+                var point = new BMap.Point(points[i].lng, points[i].lat);
+                var marker = new BMap.Marker(point);
+                map.addOverlay(marker);
+            }
+        });
+    }
+}
