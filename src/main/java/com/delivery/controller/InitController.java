a80604dd28216bdefc44b9a2311b5aa985af775b
OrangeHuang <OrangeHuang@users.noreply.github.com>
2019-02-24 10:56:39 +0800

    20190224 早

--- src/main/java/com/delivery/controller/InitController.java
+++ src/main/java/com/delivery/controller/InitController.java
@@ -1,6 +1,15 @@
 package com.delivery.controller;
 
+import com.delivery.entity.PointOfInterest;
+import com.delivery.entity.Worker;
+import com.delivery.service.InitService;
+import com.delivery.service.WorkerService;
+import com.delivery.utils.LngLatRange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Administrator
@@ -10,7 +19,27 @@ import org.springframework.stereotype.Controller;
  * @date 2019/2/23 17:16
  */
 
-@Controller
+@RequestMapping("/initial")
+@RestController
 public class InitController {
     // 初始化地图界面
+    private final InitService initService;
+
+    @Autowired
+    public InitController(InitService initService) {
+        this.initService = initService;
+    }
+
+    @RequestMapping(value="/workers", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Worker> getWorkers(@RequestBody LngLatRange llRange){
+        return initService.initWorkers(llRange);
+
+    }
+
+    @RequestMapping(value="pointOfInterests", method = RequestMethod.GET)
+    @ResponseBody
+    public List<PointOfInterest> getPointOfInterest(@RequestBody LngLatRange llRange){
+        return initService.initPointOfInterests(llRange);
+    }
 }
